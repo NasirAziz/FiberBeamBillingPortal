4e1b23f1789ad6157250f1a490aab81a5cf69f5c
Abdul haseeb <jasialnew1@gmail.com>
2021-06-23 16:57:55 +0500

    layout

--- app/src/main/java/com/example/fiberbeamportal/User.kt
+++ app/src/main/java/com/example/fiberbeamportal/User.kt
@@ -7,5 +7,6 @@ class User : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+        supportActionBar?.title = "User Login"
     }
 }
